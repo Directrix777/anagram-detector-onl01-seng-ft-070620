b0823a09eb5eca4a11112515e3e215e782f6ce2e
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-17 16:13:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+  def initialize(string)
+    @word = string
+  end
+
+  def match(array)
+    word_alpha = @word.chars.sort
+    matches = []
+    #alphabetizing the letters in our word.
+    array.each{|anagram| matches << anagram if anagram.chars.sort == word_alpha}
+    #alphabatizing the letters of each possible match,
+    #then comparing each alphabetized string to our alphabetized word
+    matches
+  end
+
+end
